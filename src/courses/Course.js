06ed57982bcaac547bc51ff6453078d089e2f67a
ChaimaSahli <st.chaima25@gmail.com>
2024-02-11 14:02:26 +0000

    listing all courses and delete course work

--- src/courses/Course.js
+++ src/courses/Course.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../sidebar/Sidebar";
 import "./course.css";
 import { Button } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CreateIcon from "@mui/icons-material/Create";
+import {  Modal } from 'antd';
 
 function Course() {
+  const [courses, setCourses] = useState([]);
+
+  //! function to get list of courses
+  const getCourses = async () => {
+    try {
+      const data = await axios.get("http://localhost:5002/get_all_courses");
+      // console.log(data.data);
+      setCourses(data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getCourses();
+  }, []);
+
+
+  //! function to delete a course 
+  const delete_course = (courseID) => {
+    Modal.confirm({
+      title:'Delete Course',
+      content:'Are you sure you want to delete this Course?',
+      onOk(){
+        axios.delete(`http://localhost:5002/delete_course/${courseID}`)
+        .then(() => {
+          window.location.reload(false);
+
+        })
+      },
+      okText:'Delete',
+      cancelText:'Cancel',
+      okButtonProps:{
+        style: {backgroundColor: '#C10000'}
+      }      
+
+    })
+  }
+
+
   return (
     <div className='course'>
       <div className='container'>
@@ -21,7 +65,7 @@ function Course() {
             <p>Hello, add your courses and view them here</p>
             <img src='assets/typing.png' alt='typing' width={"190px"} />
           </div>
-        <Link to={'/addcourse'}> 
+          <Link to={"/addcourse"}>
             <Button
               variant='contained'
               style={{
@@ -34,45 +78,22 @@ function Course() {
             >
               Add Course
             </Button>
-          </Link>  
+          </Link>
           <div className='courses-box-container'>
-            <div className='course-card'>
-              <div className='course-card-content'>
-                <p className='heading'>Java</p>
-                <p className='description'>it's java programming language</p>
-                <button className='category'>Programming langauge</button>
+            {courses.map((course) => (
+              <div className='course-card' key={course.id}>
+                <div className='course-card-content'>
+                  <p className='heading'>{course.title}</p>
+                  <p className='description'>{course.description}</p>
+                  <button className='category'>{course.category}</button>
+                  <div className='buttons-card'>
+                   
+                    <DeleteIcon onClick={() => delete_course(course._id)}  style={{ color: "red", cursor:"pointer" }} />
+                    <CreateIcon style={{ color: "#35e9bc", cursor:"pointer"  }} className="penicon" />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className='course-card'>
-              <div className='course-card-content'>
-                <p className='heading'>Python</p>
-              </div>
-            </div>
-            <div className='course-card'>
-              <div className='course-card-content'>
-                <p className='heading'>Python</p>
-              </div>
-            </div>
-            <div className='course-card'>
-              <div className='course-card-content'>
-                <p className='heading'>Python</p>
-              </div>
-            </div>
-            <div className='course-card'>
-              <div className='course-card-content'>
-                <p className='heading'>Python</p>
-              </div>
-            </div>
-            <div className='course-card'>
-              <div className='course-card-content'>
-                <p className='heading'>Python</p>
-              </div>
-            </div>
-            <div className='course-card'>
-              <div className='course-card-content'>
-                <p className='heading'>Python</p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
