4eef8898a4ee7830a1a903d5c53082bba4224cb1
ChaimaSahli <st.chaima25@gmail.com>
2024-02-14 20:28:02 +0000

    update course works and sidebar ui

--- src/courses/Course.js
+++ src/courses/Course.js
@@ -7,10 +7,22 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CreateIcon from "@mui/icons-material/Create";
-import {  Modal } from 'antd';
+import { Modal } from "antd";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// import { useForm } from "react-hook-form";
+
+//dialogue mui
+import { Dialog } from "@mui/material";
+import { DialogTitle } from "@mui/material";
+import { DialogContent } from "@mui/material";
 
 function Course() {
   const [courses, setCourses] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [courseID, setCourseID] = useState("");
 
   //! function to get list of courses
   const getCourses = async () => {
@@ -27,31 +39,157 @@ function Course() {
     getCourses();
   }, []);
 
+  //! function to get one course
+  const get_one_course = (courseID) => {
+    axios
+      .get(`http://localhost:5002/get_course/${courseID}`)
+      .then((res) => {
+        setTitle(res.data.title);
+        setDescription(res.data.description);
+        setCategory(res.data.category);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
 
-  //! function to delete a course 
+  //! function to delete a course
   const delete_course = (courseID) => {
     Modal.confirm({
-      title:'Delete Course',
-      content:'Are you sure you want to delete this Course?',
-      onOk(){
-        axios.delete(`http://localhost:5002/delete_course/${courseID}`)
-        .then(() => {
-          window.location.reload(false);
-
-        })
+      title: "Delete Course",
+      content: "Are you sure you want to delete this Course?",
+      onOk() {
+        axios
+          .delete(`http://localhost:5002/delete_course/${courseID}`)
+          .then(() => {
+            window.location.reload(false);
+          });
       },
-      okText:'Delete',
-      cancelText:'Cancel',
-      okButtonProps:{
-        style: {backgroundColor: '#C10000'}
-      }      
+      okText: "Delete",
+      cancelText: "Cancel",
+      okButtonProps: {
+        style: { backgroundColor: "#C10000" },
+      },
+    });
+  };
 
-    })
-  }
+  //controling form
+  // const{register, handleSubmit, formState : {errors}} = useForm({
+  //   defaultValues:{
+  //     title:'',
+  //     description:'',
+  //     category:'',
 
+  //   }
+  // })
+
+  //notify
+  const notify_edit = () => {
+    toast.success(
+      " Course successfully updated !",
+      {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: "light",
+      },
+      { toastId: "successNotif" }
+    );
+  };
+  // ! function to update a course
+  const update_course = () => {
+    axios
+      .patch(`http://localhost:5002/update_course/${courseID}`, {
+        title: title,
+        description: description,
+        category: category,
+      })
+      .then((res) => {
+        console.log(res);
+        notify_edit();
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err.response.data.err);
+      });
+  };
+
+  //! opening and closing edit interview dialog
+  const [openEdit, setOpenEdit] = useState(false);
+  const handleEditDialog = (courseID) => {
+    setOpenEdit(true);
+    get_one_course(courseID);
+  };
+  const closeEditDialog = () => {
+    setOpenEdit(false);
+  };
 
   return (
     <div className='course'>
+      {/* //! dialog edit course */}
+      <Dialog
+        open={openEdit}
+        onClose={closeEditDialog}
+        className='dialog'
+        PaperProps={{ sx: { width: "30%", height: "62%" } }}
+      >
+        <DialogTitle className='dialog-title'>Edit Course details</DialogTitle>
+        <DialogContent className='dialog-content'>
+          <form onSubmit={update_course} className='form-edit'>
+            <div className='form_group'>
+              <label className='sub_title_edit' for='title'>
+                Title
+              </label>
+              <input
+                placeholder='Enter The course title'
+                className='form_style'
+                type='text'
+                name='title'
+                value={title}
+                onChangeCapture={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className='form_group'>
+              <label className='sub_title_edit' for='description'>
+                Description
+              </label>
+              <textarea
+                placeholder='Enter The course description'
+                className='form_style'
+                type='text'
+                name='description'
+                value={description}
+                onChangeCapture={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className='form_group'>
+              <label className='sub_title_edit' for='category'>
+                Category
+              </label>
+              <input
+                placeholder='Enter The course category'
+                className='form_style'
+                name='category'
+                type='text'
+                value={category}
+                onChangeCapture={(e) => setCategory(e.target.value)}
+              />
+            </div>
+            <div className='dialog-buttons'>
+              <button onClick={closeEditDialog} className='dialog-cancel'>
+                Cancel
+              </button>
+              <button className='dialog-save'>Update Course</button>
+            </div>
+          </form>
+          <ToastContainer />
+        </DialogContent>
+      </Dialog>
       <div className='container'>
         <Sidebar />
         <div className='main'>
@@ -87,9 +225,18 @@ function Course() {
                   <p className='description'>{course.description}</p>
                   <button className='category'>{course.category}</button>
                   <div className='buttons-card'>
-                   
-                    <DeleteIcon onClick={() => delete_course(course._id)}  style={{ color: "red", cursor:"pointer" }} />
-                    <CreateIcon style={{ color: "#35e9bc", cursor:"pointer"  }} className="penicon" />
+                    <DeleteIcon
+                      onClick={() => delete_course(course._id)}
+                      style={{ color: "red", cursor: "pointer" }}
+                    />
+                    <CreateIcon
+                      style={{ color: "#35e9bc", cursor: "pointer" }}
+                      className='penicon'
+                      onClick={() => {
+                        setCourseID(course._id);
+                        handleEditDialog(course._id);
+                      }}
+                    />
                   </div>
                 </div>
               </div>
--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,45 +1,77 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './sidebar.css'
-import HomeIcon from '@mui/icons-material/Home';
-import AutoStoriesIcon from '@mui/icons-material/AutoStories';
-import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
-import PlayLessonIcon from '@mui/icons-material/PlayLesson';
-import QuizIcon from '@mui/icons-material/Quiz';
-import AssessmentIcon from '@mui/icons-material/Assessment';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./sidebar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import PlayLessonIcon from "@mui/icons-material/PlayLesson";
+import QuizIcon from "@mui/icons-material/Quiz";
+import AssessmentIcon from "@mui/icons-material/Assessment";
 function Sidebar() {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  }
+
   return (
     <div className='sidebar-menu'>
-      <div className='side-logo'> 
-        <h1 style={{color:"#35e9bc"}}>CodeLingo</h1>
-      </div>    
+      <div className='side-logo'>
+        <h1 style={{ color: "#35e9bc" }}>CodeLingo</h1>
+      </div>
 
       <ul>
-        <li className='selected'>
-            <Link to={'/'}><i><HomeIcon style={{width:"60px"}}/></i><span>Dashboard</span></Link>
+        <li className={isActive('/') ? 'selected' : ''}>
+          <Link to={"/"}>
+            <i>
+              <HomeIcon style={{ width: "60px" }} />
+            </i>
+            <span>Dashboard</span>
+          </Link>
+        </li>
+        <li className={isActive('/courses') ? 'selected' : ''}>
+          <Link to={"/courses"}>
+            <i>
+              <AutoStoriesIcon style={{ width: "60px" }} />
+            </i>
+            <span>Courses</span>
+          </Link>
+        </li>
+        <li className={isActive('/manageUnits') ? 'selected' : ''}>
+          <Link to={"/manageUnits"}>
+            <i>
+              <LocalLibraryIcon style={{ width: "60px" }} />
+            </i>
+            <span>Units</span>
+          </Link>
         </li>
         <li>
-            <Link to={'/courses'}><i><AutoStoriesIcon style={{width:"60px"}}/></i><span>Courses</span></Link>
-
+          <Link to={"/"}>
+            <i>
+              <PlayLessonIcon style={{ width: "60px" }} />
+            </i>
+            <span>Lessons</span>
+          </Link>
         </li>
         <li>
-            <Link to={'/'}><i><LocalLibraryIcon style={{width:"60px"}}/></i><span>Units</span></Link>
-
+          <Link to={"/"}>
+            <i>
+              <QuizIcon style={{ width: "60px" }} />
+            </i>
+            <span>Exercises</span>
+          </Link>
         </li>
         <li>
-            <Link to={'/'}><i ><PlayLessonIcon style={{width:"60px"}}/></i><span>Lessons</span></Link>
-
-        </li>
-        <li>
-            <Link to={'/'}><i><QuizIcon style={{width:"60px"}}/></i><span>Exercises</span></Link>
-        </li>
-         <li>
-            <Link to={'/'}><i ><AssessmentIcon style={{width:"60px"}}/></i><span>Reports</span></Link>
+          <Link to={"/"}>
+            <i>
+              <AssessmentIcon style={{ width: "60px" }} />
+            </i>
+            <span>Reports</span>
+          </Link>
         </li>
       </ul>
-      
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
